b163bb33476486b9b05af17b32582b8572642b07
teunmooij <tmooij@infinitaslearning.com>
2022-11-30 21:17:55 +0100

    move global declaration for type recognition

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
+export {};
+import { fnNested, NestingMock } from './mocks/nestingMock';
+import { fnCurried, CurryMock } from './mocks/curryMock';
+import { nestingArgs, NestingArgs } from './helpers/args';
+
+export { fnNested, NestingMock, fnCurried, CurryMock, nestingArgs, NestingArgs };
+
 export { toHaveBeenNestedCalledWith } from './matchers/toHaveBeenNestedCalledWith';
-export { fnNested, NestingMock } from './mocks/nestingMock';
-export { fnCurried, CurryMock } from './mocks/curryMock';
-export { nestingArgs, NestingArgs } from './helpers/args';
 export { init } from './init';
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      /**
+       * Performs expectation on the nested mock the have been called
+       * @param {NestingArgs|any[][]} args Consecutive args expected to have been called on the mock
+       */
+      toHaveBeenNestedCalledWith(nestedArgs: NestingArgs | any[][]): R;
+      /**
+       * Performs expectation on the nested mock the have been called
+       * @param {NestingArgs|any[][]} args Consecutive args expected to have been called on the mock
+       */
+      toBeNestedCalledWith(nestedArgs: NestingArgs | any[][]): R;
+    }
+  }
+
+  // eslint-disable-next-line no-var
+  var nest: {
+    fn: typeof fnNested;
+    chain: typeof fnNested;
+    curry: typeof fnCurried;
+    args: typeof nestingArgs;
+  };
+}
--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,6 @@
 import { fnNested } from './mocks/nestingMock';
 import { fnCurried } from './mocks/curryMock';
-import { nestingArgs, NestingArgs } from './helpers/args';
+import { nestingArgs } from './helpers/args';
 import { toHaveBeenNestedCalledWith } from './matchers/toHaveBeenNestedCalledWith';
 
 export const init = () => {
@@ -11,27 +11,8 @@ export const init = () => {
 
   global.nest = {
     fn: fnNested,
+    chain: fnNested,
     curry: fnCurried,
     args: nestingArgs,
   };
 };
-
-export {};
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      /**
-       * Performs expectation on the nested mock the have been called
-       * @param {NestingArgs|any[][]} args Consecutive args expected to have been called on the mock
-       */
-      toHaveBeenNestedCalledWith(nestedArgs: NestingArgs | any[][]): R;
-    }
-  }
-
-  // eslint-disable-next-line no-var
-  var nest: {
-    fn: typeof fnNested;
-    curry: typeof fnCurried;
-    args: typeof nestingArgs;
-  };
-}
