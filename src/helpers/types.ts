d60494ea8db3675bb7ea211c55fd14a41bf1b1d3
teunmooij <tmooij@infinitaslearning.com>
2023-01-22 12:55:15 +0100

    refactored WithPath to support functions in the path

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -7,11 +7,19 @@ type AsPath<T> = T extends Path ? T : never;
 type Root<P extends Path> = AsKey<P extends Readonly<[infer R, ...any[]]> ? R : never>;
 type Rest<P extends Path> = AsPath<P extends Readonly<[any, ...infer R]> ? R : never>;
 
-type BuildPath<P extends Path, V> = {
-  [x in Root<P>]: Rest<P> extends [] ? V : BuildPath<Rest<P>, V>;
+export type WithPath<Shape, P extends Path, V> = Shape & {
+  [x in Root<P>]: Rest<P> extends []
+    ? V
+    : x extends keyof Shape
+    ? Shape[x] extends (...args: any[]) => any
+      ? (
+          ...args: Parameters<Shape[x]>
+        ) => ReturnType<Shape[x]> extends PromiseLike<infer R>
+          ? Promise<WithPath<P, Rest<R>, V>>
+          : WithPath<ReturnType<Shape[x]>, Rest<P>, V>
+      : WithPath<Shape[x], Rest<P>, V>
+    : never;
 };
-
-export type WithPath<Shape extends Record<string, unknown>, P extends Path, V> = Shape & BuildPath<P, V>;
 
 export interface CallState {
   callPath: any[][];
--- test/helpers/types.spec.ts
+++ test/helpers/types.spec.ts
@@ -2,15 +2,15 @@ import { WithPath } from '../../src/helpers/types';
 import { expectTypes } from '../utils';
 
 describe('type tests', () => {
-  it('compiles if all types match', () => {
+  it('supports adding simple and object types', () => {
     // Arrange
     type Shape = { foo: { bar: string; baz: { fred: number } } };
+
     type ExpectedWithQux = { foo: { bar: string; baz: { fred: number } }; qux: string };
     type ExpectedWithQuux = { foo: { bar: string; baz: { fred: number; quux: string } }; qux: string };
     type ExpectedWithCorge = { foo: { bar: string; baz: { fred: number; quux: string }; corge: boolean }; qux: string };
     type ExpectedWithGarply = {
-      foo: { bar: string; baz: { fred: number; quux: string }; corge: boolean };
-      qux: string;
+      foo: { bar: string; baz: { fred: number } };
       grault: { graply: number[] };
     };
 
@@ -18,7 +18,57 @@ describe('type tests', () => {
     type ActualWithQux = WithPath<Shape, ['qux'], string>;
     type ActualWithQuux = WithPath<ActualWithQux, ['foo', 'baz', 'quux'], string>;
     type ActualWithCorge = WithPath<ActualWithQuux, ['foo', 'corge'], boolean>;
-    type ActualWithGarply = WithPath<ActualWithCorge, ['grault', 'graply'], number[]>;
+    type ActualWithGarply = WithPath<Shape, ['grault'], { graply: number[] }>;
+
+    // Assert
+    expectTypes<ActualWithQux, ExpectedWithQux>().toBeIdentical;
+    expectTypes<ActualWithQuux, ExpectedWithQuux>().toBeIdentical;
+    expectTypes<ActualWithCorge, ExpectedWithCorge>().toBeIdentical;
+    expectTypes<ActualWithGarply, ExpectedWithGarply>().toBeIdentical;
+  });
+
+  it('does not allow nested subpaths under non-existing keys', () => {
+    type Shape = { foo: { bar: string; baz: { fred: number } } };
+
+    type ExpectedWithGarply = {
+      foo: { bar: string; baz: { fred: number } };
+      grault: never;
+    };
+
+    type ActualWithGarply = WithPath<Shape, ['grault', 'graply'], number[]>;
+
+    expectTypes<ActualWithGarply, ExpectedWithGarply>().toBeIdentical;
+  });
+
+  it('extends paths as the return types of the mock functions', () => {
+    // Arrange
+    type Shape = { foo: () => { bar: (arg: number) => string; baz: (arg0: boolean, arg1: string) => { fred: number } } };
+    type ExpectedWithQux = {
+      foo: () => { bar: (arg: number) => string; baz: (arg0: boolean, arg1: string) => { fred: number } };
+      qux: string;
+    };
+    type ExpectedWithQuux = {
+      foo: () => { bar: (arg: number) => string; baz: (arg0: boolean, arg1: string) => { fred: number; quux: string } };
+      qux: string;
+    };
+    type ExpectedWithCorge = {
+      foo: () => {
+        bar: (arg: number) => string;
+        baz: (arg0: boolean, arg1: string) => { fred: number; quux: string };
+        corge: (arg: string) => boolean;
+      };
+      qux: string;
+    };
+    type ExpectedWithGarply = {
+      foo: () => { bar: (arg: number) => string; baz: (arg0: boolean, arg1: string) => { fred: number } };
+      grault: { graply: number[] };
+    };
+
+    // Act
+    type ActualWithQux = WithPath<Shape, ['qux'], string>;
+    type ActualWithQuux = WithPath<ActualWithQux, ['foo', 'baz', 'quux'], string>;
+    type ActualWithCorge = WithPath<ActualWithQuux, ['foo', 'corge'], (arg: string) => boolean>;
+    type ActualWithGarply = WithPath<Shape, ['grault'], { graply: number[] }>;
 
     // Assert
     expectTypes<ActualWithQux, ExpectedWithQux>().toBeIdentical;
