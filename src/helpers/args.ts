4a2e610000e1b747e8856dc106b6bf5ea010a815
Teun Mooij <tmooij@infinitaslearning.com>
2023-01-24 21:45:36 +0100

    Support object mock in nest.args

--- src/helpers/args.ts
+++ src/helpers/args.ts
@@ -8,12 +8,19 @@ export type NestingArgs = {
    * List of the expected call arguments for each level
    */
   args: any[][];
-};
+} & Record<string | number | symbol, (...args: any[]) => NestingArgs>;
 
 const createNestingArgs = (args: any[][]): NestingArgs => {
-  const fn = (...next: any[]) => createNestingArgs([...args, next]) as NestingArgs;
+  const fn = (...next: any[]) => createNestingArgs([...args, next]);
   fn.args = args;
-  return fn;
+
+  return new Proxy(fn, {
+    apply: (target, _thisArg, args) => target(...args),
+    get: (target, name) => {
+      if (name === 'args') return target.args;
+      return (...args: any[]) => target(name, ...args);
+    },
+  }) as unknown as NestingArgs;
 };
 
 /**
@@ -21,4 +28,4 @@ const createNestingArgs = (args: any[][]): NestingArgs => {
  * @param {...any[]} args expected arguments for the top level call
  * @returns {NestingArgs} Arguments for the next level of depth
  */
-export const nestingArgs = (...args: any[]) => createNestingArgs([args]);
+export const nestingArgs = createNestingArgs([]);
